77a5f438780711dfb943db7ecfdfa6cf33ecd94d
icylisper <icylisper@gmail.com>
2019-01-23 10:39:03 -0800

    cache ssm get; put invalidates cache

---
+++ src/ednism/cache.clj
@@ -0,0 +1,12 @@
+(ns ednism.cache
+  (:require
+   [clojure.core.cache :as cache]))
+
+(def C (atom (cache/fifo-cache-factory {})))
+
+(defn lookup! [k f]
+  (swap! C cache/through-cache k f)
+  (get @C k))
+
+(defn invalidate! [k]
+  (swap! C dissoc k))
--- src/ednism/core.clj
+++ src/ednism/core.clj
@@ -5,16 +5,18 @@
    [saw.core :as saw]
    [ednism.store.core :as store]
    [ednism.store.file :as file]
-   [ednism.store.ssm :as ssm]))
+   [ednism.store.ssm :as ssm]
+   [ednism.cache :as cache]))
 
 (defn put-kv [k v]
   (store/put-kv k v))
 
 (defn put [path m]
-  (store/put path m))
+  (store/put path m)
+  (cache/invalidate! path))
 
 (defn get [path]
-  (store/get path))
+  (cache/lookup! path store/get))
 
 (defn history [path]
   (store/history path))
